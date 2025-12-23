b8b01bb7ce57d6c879dd71e2f5be1b8420464d1b
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-24 00:32:24 +0900

    Configure orval

--- crates/app/src/bin/export_schema.rs
+++ crates/app/src/bin/export_schema.rs
@@ -5,7 +5,7 @@ use anyhow::Result;
 fn main() -> Result<()> {
     let (_, openapi) = app::create_app();
 
-    fs::write("openapi.json", openapi.to_pretty_json()?)?;
+    fs::write("api/openapi.json", openapi.to_pretty_json()?)?;
 
     Ok(())
 }
