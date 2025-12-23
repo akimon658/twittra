8afc7807081887a2bce27719a80ec0323e7eaf6c
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-23 23:05:50 +0900

    Generate openapi.json

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
+use axum::Router;
 use tokio::net::TcpListener;
-use utoipa::openapi::{Info, OpenApiBuilder, Server};
+use utoipa::openapi::{Info, OpenApi, OpenApiBuilder, Server};
 use utoipa_axum::router::OpenApiRouter;
 use utoipa_swagger_ui::SwaggerUi;
 
@@ -8,9 +9,9 @@ use crate::handler::user;
 
 mod handler;
 
-pub async fn serve() -> Result<()> {
+pub fn create_app() -> (Router, OpenApi) {
     const API_ROOT: &str = "/api/v1";
-    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+
     let openapi = OpenApiBuilder::new()
         .info(Info::new("Twittra", env!("CARGO_PKG_VERSION")))
         .servers(Some([Server::new(API_ROOT)]))
@@ -18,9 +19,16 @@ pub async fn serve() -> Result<()> {
     let (router, openapi) = OpenApiRouter::with_openapi(openapi)
         .routes(utoipa_axum::routes!(user::get_me))
         .split_for_parts();
-    let router = axum::Router::new()
-        .nest(API_ROOT, router)
-        .merge(SwaggerUi::new("/docs/swagger-ui").url("/docs/openapi.json", openapi));
+    let router = axum::Router::new().nest(API_ROOT, router);
+
+    (router, openapi)
+}
+
+pub async fn serve() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+    let (router, openapi) = create_app();
+    let router =
+        router.merge(SwaggerUi::new("/docs/swagger-ui").url("/docs/openapi.json", openapi));
 
     axum::serve(listener, router).await?;
 
