7ce3f7038639fc9408905935e64b9dc57273ff9a
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-23 22:05:30 +0900

    Configure utoipa

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
-use axum::{Router, routing};
 use tokio::net::TcpListener;
+use utoipa::openapi::{Info, OpenApiBuilder, Server};
+use utoipa_axum::router::OpenApiRouter;
+use utoipa_swagger_ui::SwaggerUi;
 
 use crate::handler::user;
 
@@ -8,9 +10,21 @@ mod handler;
 
 pub async fn serve() -> Result<()> {
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
-    let router = Router::new().route("/api/v1/me", routing::get(user::get_me));
+    let openapi = OpenApiBuilder::new()
+        .info(Info::new("Twittra", env!("CARGO_PKG_VERSION")))
+        .servers(Some([Server::new("/api/v1")]))
+        .build();
+    let (router, openapi_with_routes) = OpenApiRouter::with_openapi(openapi)
+        .routes(utoipa_axum::routes!(user::get_me))
+        .split_for_parts();
 
-    axum::serve(listener, router).await?;
+    axum::serve(
+        listener,
+        router.merge(
+            SwaggerUi::new("/docs/swagger-ui").url("/docs/openapi.json", openapi_with_routes),
+        ),
+    )
+    .await?;
 
     Ok(())
 }
