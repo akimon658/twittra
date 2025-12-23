ed7cc7880973ff933cc77e6357eec1740d0cd3cb
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-23 22:32:04 +0900

    Properly next axum router path

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -9,22 +9,20 @@ use crate::handler::user;
 mod handler;
 
 pub async fn serve() -> Result<()> {
+    const API_ROOT: &str = "/api/v1";
     let listener = TcpListener::bind("0.0.0.0:8080").await?;
     let openapi = OpenApiBuilder::new()
         .info(Info::new("Twittra", env!("CARGO_PKG_VERSION")))
-        .servers(Some([Server::new("/api/v1")]))
+        .servers(Some([Server::new(API_ROOT)]))
         .build();
-    let (router, openapi_with_routes) = OpenApiRouter::with_openapi(openapi)
+    let (router, openapi) = OpenApiRouter::with_openapi(openapi)
         .routes(utoipa_axum::routes!(user::get_me))
         .split_for_parts();
+    let router = axum::Router::new()
+        .nest(API_ROOT, router)
+        .merge(SwaggerUi::new("/docs/swagger-ui").url("/docs/openapi.json", openapi));
 
-    axum::serve(
-        listener,
-        router.merge(
-            SwaggerUi::new("/docs/swagger-ui").url("/docs/openapi.json", openapi_with_routes),
-        ),
-    )
-    .await?;
+    axum::serve(listener, router).await?;
 
     Ok(())
 }
