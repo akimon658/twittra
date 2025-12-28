b7e58bf99928865a8712dacb8bbe5786b453f9bb
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-29 01:49:07 +0900

    Add global error boundary

--- app/App.tsx
+++ app/App.tsx
@@ -1,6 +1,15 @@
-import { createTheme, MantineProvider } from "@mantine/core"
+import {
+  Alert,
+  Center,
+  Code,
+  Container,
+  createTheme,
+  MantineProvider,
+} from "@mantine/core"
 import "@mantine/core/styles.css"
+import { IconExclamationCircle } from "@tabler/icons-react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ErrorBoundary, type FallbackProps } from "react-error-boundary"
 import { AuthProvider } from "../auth/components/AuthProvider.tsx"
 import { useUser } from "../auth/hooks/useUser.ts"
 import "./global.css"
@@ -16,14 +25,36 @@ const theme = createTheme({
   fontFamily: "Google Sans Flex, Noto Sans JP, sans-serif",
 })
 
+const GlobalErrorFallback = ({ error }: FallbackProps) => {
+  return (
+    <Container>
+      <Center h="100dvh">
+        <Alert
+          color="red"
+          icon={<IconExclamationCircle />}
+          title="予期しないエラーが発生しました"
+        >
+          <Code block>
+            {error instanceof Error
+              ? (error.stack ?? error.message)
+              : String(error)}
+          </Code>
+        </Alert>
+      </Center>
+    </Container>
+  )
+}
+
 export const App = () => {
   return (
     <MantineProvider defaultColorScheme="auto" theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <Greet />
-        </AuthProvider>
-      </QueryClientProvider>
+      <ErrorBoundary FallbackComponent={GlobalErrorFallback}>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <Greet />
+          </AuthProvider>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </MantineProvider>
   )
 }
