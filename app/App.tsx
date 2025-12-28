9ad3632f4752f9fb34713794b0df723c6504a552
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-28 21:41:26 +0900

    Add Mantine provider

--- app/App.tsx
+++ app/App.tsx
@@ -1,3 +1,5 @@
+import { MantineProvider } from "@mantine/core"
+import "@mantine/core/styles.css"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Suspense } from "react"
 import { useGetMeSuspense } from "../api/user/user.ts"
@@ -6,13 +8,15 @@ const queryClient = new QueryClient()
 
 export const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <a href="/api/v1/auth/login">Login</a>
+    <MantineProvider>
+      <QueryClientProvider client={queryClient}>
+        <a href="/api/v1/auth/login">Login</a>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Greet />
-      </Suspense>
-    </QueryClientProvider>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Greet />
+        </Suspense>
+      </QueryClientProvider>
+    </MantineProvider>
   )
 }
 
