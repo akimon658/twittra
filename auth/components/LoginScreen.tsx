b9d2dd0849a6703ef3e6039b7a434beeb372c98d
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-12-28 23:24:51 +0900

    Add traQ logo

--- auth/components/LoginScreen.tsx
+++ auth/components/LoginScreen.tsx
@@ -1,10 +1,17 @@
 import { Button, Center, Container } from "@mantine/core"
+import { TraqLogo } from "../../components/TraqLogo.tsx"
 
 export const LoginScreen = () => {
   return (
     <Container>
       <Center h="100dvh">
-        <Button component="a" fullWidth href="/api/v1/auth/login" size="xl">
+        <Button
+          component="a"
+          fullWidth
+          href="/api/v1/auth/login"
+          leftSection={<TraqLogo size={24} />}
+          size="xl"
+        >
           traQでログイン
         </Button>
       </Center>
