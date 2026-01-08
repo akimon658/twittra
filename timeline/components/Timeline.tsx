9f628ef418c92fc071ecfa558013b23928da97b0
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-01-08 14:22:09 +0900

    Add `TimelineFallback` component

--- timeline/components/Timeline.tsx
+++ timeline/components/Timeline.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mantine/core"
+import { Group, Paper, Skeleton, Stack } from "@mantine/core"
 import { Suspense } from "react"
 import { useGetTimelineSuspense } from "../../api/timeline/timeline.ts"
 import { MessageItem } from "./Message.tsx"
@@ -13,9 +13,38 @@ const TimelineContent = () => {
   )
 }
 
+const TimelineFallback = () => {
+  return (
+    <Stack
+      h="calc(100dvh - var(--mantine-spacing-md))"
+      // Cancel out the padding of AppShell
+      mb="-md"
+      style={{ overflow: "hidden" }}
+    >
+      {Array.from({ length: 10 }).map((_, index) => (
+        <Paper key={index}>
+          <Group align="start" wrap="nowrap">
+            <Skeleton circle height={40} />
+
+            <Stack flex={1} gap="xs">
+              <Skeleton height="1rem" width="10rem" />
+
+              <Skeleton height="1rem" />
+
+              <Skeleton height="1rem" />
+
+              <Skeleton height="1rem" width="80%" />
+            </Stack>
+          </Group>
+        </Paper>
+      ))}
+    </Stack>
+  )
+}
+
 export const Timeline = () => {
   return (
-    <Suspense fallback={<div>Loading timeline...</div>}>
+    <Suspense fallback={<TimelineFallback />}>
       <TimelineContent />
     </Suspense>
   )
