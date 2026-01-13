1e9ee1cfde8418762be4b1fafa2eda4541aafaab
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-01-13 22:34:42 +0900

    chore: Group reaction endpoints

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -48,8 +48,10 @@ pub fn setup_openapi_routes() -> Result<(Router<AppState>, OpenApi)> {
     let openapi_router = OpenApiRouter::with_openapi(openapi)
         .routes(utoipa_axum::routes!(auth::login))
         .routes(utoipa_axum::routes!(auth::oauth_callback))
-        .routes(utoipa_axum::routes!(message::add_message_stamp))
-        .routes(utoipa_axum::routes!(message::remove_message_stamp))
+        .routes(utoipa_axum::routes!(
+            message::add_message_stamp,
+            message::remove_message_stamp
+        ))
         .routes(utoipa_axum::routes!(stamp::get_stamp_by_id))
         .routes(utoipa_axum::routes!(stamp::get_stamps))
         .routes(utoipa_axum::routes!(stamp::get_stamp_image))
