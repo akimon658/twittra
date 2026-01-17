5fefb720dab9f87002e37b41bbc3ccd201bc1645
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2026-01-17 22:39:35 +0900

    Fix sloppy imports

--- api/mocks.ts
+++ api/mocks.ts
@@ -1,12 +1,12 @@
 // MSW handlers aggregated from all API endpoints
-import { getTimelineMock } from "./timeline/timeline.msw"
-import { getUserMock } from "./user/user.msw"
-import { getStampMock } from "./stamp/stamp.msw"
-import { getMessageMock } from "./message/message.msw"
+import { getMessageMock } from "./message/message.msw.ts"
+import { getStampMock } from "./stamp/stamp.msw.ts"
+import { getTimelineMock } from "./timeline/timeline.msw.ts"
+import { getUserMock } from "./user/user.msw.ts"
 
 export const handlers = [
+  ...getMessageMock(),
+  ...getStampMock(),
   ...getTimelineMock(),
   ...getUserMock(),
-  ...getStampMock(),
-  ...getMessageMock(),
 ]
